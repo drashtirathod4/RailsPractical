73fbead1e1e1137022d0f3ed8a537f89371b24a9
drashtirathod21 <drashtir9@gmail.com>
2022-01-12 11:55:34 +0530

    Feat: Implemented CRUD operation on Product Model

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  root "articles#index"
+  resources :products
+  # root "articles#index"
 
   get "/articles", to: "articles#index"
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Product.create(name: 'Watch', desc: 'Apple Smart Watch')
+Product.create(name: 'Phone', desc: 'Samsung Smart Phone')
